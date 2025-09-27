ba0c30b6d78b11c7e004e36eed9fbbd285f3da06
matveykaishaev26 <kaishaevmatvei@gmail.com>
2025-09-27 14:08:49 +0500

    vercel

--- src/api/data.ts
+++ src/api/data.ts
@@ -1,5 +1,5 @@
 export async function fetchData(endpoint: string) {
-  const res = await fetch(`${import.meta.env.VITE_API_HOST}/api${endpoint}&key=${import.meta.env.VITE_API_KEY}`, {});
+  const res = await fetch(`/api${endpoint}&key=${import.meta.env.VITE_API_KEY}`);
   console.log(res);
   if (!res.ok) throw new Error(`Failed to fetch /${endpoint}!`);
   const data = await res.json();
